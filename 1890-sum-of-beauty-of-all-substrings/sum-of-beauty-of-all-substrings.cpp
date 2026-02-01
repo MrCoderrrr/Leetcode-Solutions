a8926979ce1ef64f5c98f1cfaff85dffd99588e7
BleedVenomX <155728429+MrCoderrrr@users.noreply.github.com>
2026-02-01 15:05:54 +0530

    Time: 278 ms (7.12%) | Memory: 29.1 MB (8.30%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int ans = 0;
+        int n = s.length();
+        for(int i = 0; i<n; i++){
+            map<char,int> hash;
+            for(int j = i; j<n; j++){
+                int a = 0;
+                int b = n;
+                hash[s[j]]++;
+                for(auto x : hash){
+                    a = max(a,x.second);
+                    b = min(b,x.second);
+                }
+                ans += a-b;
+            }
+        }
+    return ans;}
+};
