451d3c2964513b120206ae9a964eb3878d4b7e8a
BleedVenomX <155728429+MrCoderrrr@users.noreply.github.com>
2026-02-13 19:35:16 +0530

    Time: 2 ms (19.94%) | Memory: 9.4 MB (29.03%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        for(int i = 1; i<n; i++){
-            dp[0][i] = 1;
-        }
-        for(int i = 0; i<m; i++){
-            dp[i][0] = 1;
-        }
         dp[0][0] = 0;
         if(n == 1 || m == 1){
             return 1;
         }
-        for(int i = 1; i<n; i++){
-            for(int j = 1; j<m; j++){
-                dp[j][i] = dp[j-1][i] + dp[j][i-1];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(i == 0){
+                    dp[j][i] = 1;
+                }
+                else if(j == 0){
+                    dp[j][i] = 1;
+                }
+                else{
+                    dp[j][i] = dp[j-1][i] + dp[j][i-1];
+
+                }
             }
         }
     return dp[m-1][n-1];}
