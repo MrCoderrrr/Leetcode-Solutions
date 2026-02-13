c2c3687ca4d99dede949ac06d6f491626795e8bc
BleedVenomX <155728429+MrCoderrrr@users.noreply.github.com>
2026-02-13 10:36:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (34.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    int sum(int n, vector<int>& dp) {
-        if (n == 1){
-            return 1;
-            }
-        if (n == 2){
-            return 2;
-            }
-        if (dp[n] != -1){
-            return dp[n];
-            }
-
-        dp[n] = sum(n-1, dp) + sum(n-2, dp);
-        return dp[n];
-    }
-
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        int fsum = 0;
-        if(n==1){
+        if(n==0){
             return 1;
-            }
-        else if(n == 2){
-            return 2;
-            } 
-        else {
-            fsum = sum(n, dp);
-            return fsum;
-            }
-return 0;}
+        }
+        if(n == 1){
+            return 1;
+        }
+        vector<int> a(n+1);
+        a[0] = 1;
+        a[1] = 1;
+        for(int i = 2;i<=n; i++){
+            a[i] = a[i-1] + a[i-2];
+        }
+    return a[n];}
 };
