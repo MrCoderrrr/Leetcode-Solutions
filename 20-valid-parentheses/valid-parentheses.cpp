b1071413c145a241f49c391f16940efb6ae8d0f5
Xypher7979 <155728429+MrCoderrrr@users.noreply.github.com>
2026-03-21 16:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (66.95%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int n = s.length();
         stack<char> a;
-        for(int i =0; i<n; i++){
+        for(int i =0; i<s.length(); i++){
             if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 a.push(s[i]);
             }
